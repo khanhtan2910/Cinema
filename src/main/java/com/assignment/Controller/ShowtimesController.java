01e3034bc79d3e707ed8254b4506658371a760a7
ĐạtGuta <wtsdatho@gmail.com>
2023-07-16 23:24:05 +0700

    test update

--- src/main/java/com/assignment/Controller/ShowtimesController.java
+++ src/main/java/com/assignment/Controller/ShowtimesController.java
@@ -1,5 +1,12 @@
 package com.assignment.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ShowtimesController {
-	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "/hello";
+	}
 }
