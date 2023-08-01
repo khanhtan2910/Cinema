8ad7ce689095616dc4037936da1fa2aae9e46719
ĐạtGuta <wtsdatho@gmail.com>
2023-08-01 19:27:46 +0700

    xanhauquanhonhaunhieu

--- src/main/java/com/assignment/Controller/ShowtimesController.java
+++ src/main/java/com/assignment/Controller/ShowtimesController.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ShowtimesController {
+	@RequestMapping("/mamaCinema")
+	public String CinemaWeb() {
+		return "redirect:/Showtime";
+	}
 	@RequestMapping("/Showtime")
 	public String showTime(Model model) {
 		LocalDate currentdate = LocalDate.now();
